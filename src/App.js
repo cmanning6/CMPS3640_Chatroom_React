b513edd83dd16b01ba0b598aa0c5588480330b34
RodrigoOrtiz53 <rodrigoortiz53@gmail.com>
2020-11-02 18:28:13 -0800

    Firebase hosting deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import firebase from 'firebase/firestore';
+import firebase from 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyACj3_qrYUmqCant43AA6TLK4viDNPQ9zc",
+    authDomain: "cmps3640-chatroom.firebaseapp.com",
+    databaseURL: "https://cmps3640-chatroom.firebaseio.com",
+    projectId: "cmps3640-chatroom",
+    storageBucket: "cmps3640-chatroom.appspot.com",
+    messagingSenderId: "920467976054",
+    appId: "1:920467976054:web:e386f6e5197c019d01d457",
+    measurementId: "G-8925SZ5EMD"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
 function App() {
   return (
     <div className="App">
